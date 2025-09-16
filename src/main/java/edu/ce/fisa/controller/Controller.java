1a33d55d0e51df22c2b95cdb535fab3b691901f5
dlacowns21 <dlacowns21@naver.com>
2025-09-16 12:22:17 +0900

    initial commit

---
+++ src/main/java/edu/ce/fisa/controller/Controller.java
@@ -0,0 +1,21 @@
+package edu.ce.fisa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/get")
+	public String getHello() {
+		return "Get Method";
+	}
+	
+	@PostMapping("/post")
+	public String postHello() {
+		return "Post Method";
+	}
+
+
+}
